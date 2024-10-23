155be1a38e5a121e2f2d401991331068ebe15a67
Emircan <emircandv787@gmail.com>
2024-10-23 23:32:57 +0300

    The new signal methods have been added and component.ts and component.html files have been refactored.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { JsonPipe } from '@angular/common';
-import { Component, computed, ElementRef, Signal, signal, ViewChild, viewChild, WritableSignal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, ElementRef, Signal, signal, ViewChild, viewChild, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
@@ -8,9 +8,21 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, FormsModule, JsonPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+
+
+  constructor() {
+    effect((onCleanup) => {
+      let consoleMessage = `signalName has been changed new value : ${this.signalName()}`;
+
+      console.log(consoleMessage);
+
+
+    });
+  }
   numberSignal: WritableSignal<number> = signal<number>(5);
   objectSignal: WritableSignal<Person> = signal<Person>({
     name: "emir",
@@ -18,14 +30,16 @@ export class AppComponent {
     age: 22
   });
 
-  signalName: WritableSignal<string> = signal<string>("emircan");
-  signalSurname: WritableSignal<string> = signal<string>(" Koç");
+  signalName: WritableSignal<string> = signal<string>("");
+  signalSurname: WritableSignal<string> = signal<string>("");
 
   computedSignal: Signal<string> = computed<string>(() => {
     return this.signalName() + this.signalSurname();
   });
 
-  @ViewChild("input1") input1Ref!: ElementRef;
+  number: number = 0;
+  name: string = "";
+  surname: string = "";
 
 
   inrementNumberSignal() {
@@ -33,11 +47,19 @@ export class AppComponent {
   }
 
   setInput1Value(): void {
-    this.numberSignal.set(this.input1Ref.nativeElement.value);
+    this.numberSignal.set(this.number);
   }
 
   setObjectSignal(): void {
     this.objectSignal.set({ ...this.objectSignal(), age: 33 });
+  }
+
+  nameChanged() {
+    this.signalName.set(this.name);
+  }
+
+  surnameChanged() {
+    this.signalSurname.set(this.surname);
   }
 
 
